13a57bfff67b86fed8dfc6da03aa6131ba3c74e8
Abdullah-Al-Turky <47826280+coderabdullahturki@users.noreply.github.com>
2021-03-10 00:32:15 +0600

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+package hellojava;
+
+/**
+ *
+ * @author Turky
+ */
+public class HelloJava {
+    public static void main (String[] args) {
+        
+    System.out.println("Hello Java, Hello World!");
+}
+    
+}
